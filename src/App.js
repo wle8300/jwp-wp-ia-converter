9d025adaa66e1031143a8aa42507711f5c0ce652
wle8300 <email@wle8300.com>
2018-03-23 11:47:13 -0700

    asdf

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
 
 class App extends Component {
 
@@ -6,14 +7,17 @@ class App extends Component {
 
     inputValue: '',
 
+    isInputValid: false,
+
     outputValue: '',
+
+    wasEmbedCodeCopied: false,
 
   }
 
-  template = '<div class="jwp"><div id="00000000"><script src="//content.jwplatform.com/libraries/[VIDEPLAYERID].js"></script><script type="text/javascript"> var vid = jwplayer("00000000"); vid.setup({ file: "//content.jwplatform.com/videos/[VIDEOID].mp4", image: "//content.jwplatform.com/thumbs/[VIDEOID].jpg", advertising: { client: "vast", tag: "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/22633255/IA_Video&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&url= __referrer__&description_url=__page-url__&correlator=__random-number__" } }); </script></div></div>'
+  template = '<!-- ▽ JWP EMBED ▽ -->\n<div class="jwp"><div id="00000000"><script src="//content.jwplatform.com/libraries/[VIDEPLAYERID].js"></script><script type="text/javascript"> var vid = jwplayer("00000000"); vid.setup({ file: "//content.jwplatform.com/videos/[VIDEOID].mp4", image: "//content.jwplatform.com/thumbs/[VIDEOID].jpg", advertising: { client: "vast", tag: "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/22633255/IA_Video&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&url= __referrer__&description_url=__page-url__&correlator=__random-number__" } }); </script></div></div>\n<!-- △ JWP EMBED △ -->\n\n\n'
 
   render() {
-    console.log(this.state.outputValue);
     return (
       <div style={{
         display: 'flex',
@@ -39,13 +43,19 @@ class App extends Component {
           </div>
           <div>
             <textarea
-              readOnly
               name="Text1"
               rows="15"
               placeholder={this.template}
               value={this.state.outputValue}
               style={{margin: '1rem 0', padding: '1.5rem', width: '100%', fontSize: '1vw'}}
             />
+          </div>
+          <div style={{display: this.state.isInputValid ? 'block' : 'none'}}>
+            <CopyToClipboard text={this.state.outputValue} onCopy={this.handleCopy}>
+              <button style={{padding: '1rem', width: '100%', backgroundColor: this.state.wasEmbedCodeCopied ? 'rgb(64, 172, 49)' : 'inherit'}}>
+                {this.state.wasEmbedCodeCopied ? <span style={{fontWeight: 'bold', color: '#f0f0f0'}}>COPIED!</span> : 'COPY EMBED CODE'}
+              </button>
+            </CopyToClipboard>
           </div>
         </div>
       </div>
@@ -60,26 +70,26 @@ class App extends Component {
     const plugValues = (scriptSnippet, videoPlayerId, videoId) => {
 
       const uid = `jwp-${Date.now()}`
-      const x = this.template.replace(/00000000/g, uid).replace(/\[VIDEPLAYERID\]/g, videoPlayerId).replace(/\[VIDEOID\]/g, videoId)
-      console.log(x)
+
       return this.template.replace(/00000000/g, uid).replace(/\[VIDEPLAYERID\]/g, videoPlayerId).replace(/\[VIDEOID\]/g, videoId)
     }
 
-    if (!regexParsedInput || !videoPlayerId || !videoId) return this.setState({inputValue: event.target.value, outputValue: ''})
+    if (!regexParsedInput || !videoPlayerId || !videoId) return this.setState({isInputValid: false, inputValue: event.target.value, outputValue: ''})
 
     this.setState({
+      isInputValid: true,
       inputValue: event.target.value,
       outputValue: plugValues(event.target.value, videoPlayerId, videoId)
     })
+  }
 
-    // event.target.value
-    /*
-    ? does it follow the proper format, e.g. <script src="https://content.jwplatform.com/players/anIzh238-O5vsTKpz.js"></script>
+  handleCopy = () => {
 
-    get regex.replace the fields
-
-    */
-    console.log(event.target.value)
+    this.setState({wasEmbedCodeCopied: true}, () => {
+      setTimeout(() => {
+        this.setState({wasEmbedCodeCopied: false})
+      }, 2000)
+    })
   }
 }
 
